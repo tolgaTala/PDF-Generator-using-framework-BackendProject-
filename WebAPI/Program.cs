0df9b236e7d4ffacae3c674e490fe22366739a9f
tolgatala55@gmail.com <tolgatala55@gmail.com>
2022-08-25 17:11:09 +0300

    autofac problem solved

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,4 +1,10 @@
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
+using Core.Utilities.IoC;
+using Business.DependencyResolvers.Autofac;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
@@ -6,6 +12,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+// Register services directly with Autofac here. Don't
+// call builder.Populate(), that happens in AutofacServiceProviderFactory.
+builder.Host.ConfigureContainer<ContainerBuilder>(builder => builder.RegisterModule(new AutofacBusinessModule()));
 
 var app = builder.Build();
 
